04361f32eeac31e00793c89bf02663c594c1ad9c
muzammil-15 <muzammilcoder15@gmail.com>
2023-06-01 00:01:32 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import React, {useState, useEffect} from 'react';
+import { Layout } from '@/components';
+
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    
+  ) 
 }
